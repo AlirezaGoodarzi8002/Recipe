66377ec390cf9493de3b1fc52a51b12a9a9da9eb
Alireza Goodarzi <alireza.goodarzi@securitywave.co>
2024-11-02 00:47:39 +0330

    Add night mode support

--- app/src/main/java/com/security_wave/recipe/utils/BindingUtils.kt
+++ app/src/main/java/com/security_wave/recipe/utils/BindingUtils.kt
@@ -1,6 +1,7 @@
 package com.security_wave.recipe.utils
 
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
