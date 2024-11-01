b251c20e05023559340f885bb2419a7c56d444d4
Alireza Goodarzi <alireza.goodarzi@securitywave.co>
2024-11-01 18:52:05 +0330

    Add caching strategy to glide image loading

--- app/src/main/java/com/security_wave/recipe/utils/BindingUtils.kt
+++ app/src/main/java/com/security_wave/recipe/utils/BindingUtils.kt
@@ -3,6 +3,7 @@ package com.security_wave.recipe.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.security_wave.recipe.R
 
 @BindingAdapter("imageUrl")
@@ -10,6 +11,7 @@ fun loadImage(view: ImageView, imageUrl: String?) {
     imageUrl?.let {
         Glide.with(view.context)
             .load(it)
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .placeholder(R.drawable.bg_placeholder)
             .into(view)
     }
